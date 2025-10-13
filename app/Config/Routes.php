b4772c989840199a51b9e9fc963cfe388c6224cf
Paul-H18 <hornigpaul0@gmail.com>
2025-10-13 18:26:52 +0200

    Add controller classes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,10 +1,11 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use \App\Controllers\RegistrationController;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/register', 'RegistrationController::index', ['as' => 'register']);
-$routes->post('/register', 'RegistrationController::register', ['as' => 'register.registration']);
+$routes->get('/register', [RegistrationController::class, 'index'], ['as' => 'register']);
+$routes->post('/register', [RegistrationController::class, 'register'], ['as' => 'register.registration']);
