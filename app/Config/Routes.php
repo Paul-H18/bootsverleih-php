56346e7ef4c28b6c60bd85be5a9c6337432c4b26
Paul-H18 <hornigpaul0@gmail.com>
2025-10-13 18:33:05 +0200

    Refactor

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Controllers\HomeController;
 use CodeIgniter\Router\RouteCollection;
-use \App\Controllers\RegistrationController;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/register', [RegistrationController::class, 'index'], ['as' => 'register']);
-$routes->post('/register', [RegistrationController::class, 'register'], ['as' => 'register.registration']);
+
+$routes->get('/', [HomeController::class, 'index']);
