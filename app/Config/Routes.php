3560d447c0822fafdf3976d7962e2720a87d35d4
Paul <66725843+Paul-H18@users.noreply.github.com>
2025-10-13 20:34:56 +0200

    Erste stunde (#1)
    * Initial commit
    
    * Refactor
    
    * Commit
    
    * Add controller classes
    
    ---------
    
    Co-authored-by: Paul-H18 <hornigpaul0@gmail.com>

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Controllers\HomeController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', [HomeController::class, 'index']);
